40287d413d64dcef9ae3371a0bb92cd645664b98
Elie Deek <elie.d@eurisko.net>
2024-02-18 21:10:29 +0200

    chore: adding get and post

--- app/api/urlShortner/route.ts
+++ app/api/urlShortner/route.ts
@@ -1,18 +1,42 @@
 import { NextResponse } from "next/server"
+import { nanoid } from 'nanoid'
+import * as redis from 'redis';
+import { NextApiRequest, NextApiResponse } from "next";
 
 
-export async function GET(){
-    const data = {
-        name: 'elie deek ', 
-        age: '33'
-    }
+const redisClient = redis.createClient();
+redisClient.connect().catch(console.error)
 
-    return NextResponse.json({data})
+ 
+
+export async function GET(req: Request){
+    try{
+        const { searchParams } = new URL(req.url)
+        const shortenedUrl = searchParams.get('shortenedUrl') 
+    
+        if(!shortenedUrl) throw new Error('no url provided')
+
+        const originalUrl =  await redisClient.get(shortenedUrl)
+
+        return  Response.json({message: 'succcess', originalUrl})
+    }catch(e){
+        return  new Response(e?.message,{status:400})
+    } 
 }
 
-export async function POST(req, res){
-    const data = await req.json()
-    console.log(data)
-
-    return NextResponse.json(data)
+export async function POST(req: Request){
+    try{
+        const { searchParams } = new URL(req.url)
+        const originalUrl = searchParams.get('originalUrl') 
+    
+        if(!originalUrl) throw new Error('no url provided')
+    
+        const nanoidId = nanoid()
+    
+        redisClient.set(nanoidId, originalUrl)
+    
+        return  Response.json({message: 'succcess', shortenedUrl: nanoidId})
+    }catch(e){
+        return  new Response(e?.message,{status:400})
+    } 
 } 
