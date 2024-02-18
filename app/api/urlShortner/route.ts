424aad0ba3d78e9d3ab43fb725211b4b0ddd91a0
Elie Deek <elie.d@eurisko.net>
2024-02-18 21:52:37 +0200

    chore: adding documentation

--- app/api/urlShortner/route.ts
+++ app/api/urlShortner/route.ts
@@ -8,7 +8,13 @@ const redisClient = redis.createClient();
 redisClient.connect().catch(console.error)
 
  
-
+/**
+ * Retrieves the original URL associated with a shortened URL.
+ * 
+ * @param req - The NextApiRequest object representing the incoming request.
+ * @returns A Response object with a status code of 301 and a 'Location' header set to the original URL, if successful. 
+ *          Otherwise, a Response object with a status code of 400 and an error message.
+ */
 export async function GET(req: Request){
     try{
         const { searchParams } = new URL(req.url)
@@ -24,6 +30,14 @@ export async function GET(req: Request){
     } 
 }
 
+
+/**
+ * Handles the POST request to create a shortened URL.
+ * 
+ * @param req - The NextApiRequest object representing the incoming request.
+ * @returns A Promise that resolves to a NextResponse object representing the response.
+ * @throws If no original URL is provided, an error is thrown.
+ */ 
 export async function POST(req: Request){
     try{
         const { searchParams } = new URL(req.url)
