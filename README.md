4ab36bec65fbade181c5e727c80067a6ebee1d5b
Elie Deek <elie.d@eurisko.net>
2024-02-21 19:02:41 +0200

    chore: adding linting to the project

--- README.md
+++ README.md
@@ -21,7 +21,6 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
 ## Project Architecture Desgin
- 
 
 <p>
   <img src="readme/architecture_image.png" width="700" title="hover text"> 
--- app/api/urlShortner/route.ts
+++ app/api/urlShortner/route.ts
@@ -1,62 +1,61 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import * as redis from 'redis';
 
 const redisClient = redis.createClient();
-redisClient.connect().catch(console.error)
+redisClient.connect().catch(console.error);
+
+const asd = 1;
 
 /**
  * Retrieves the original URL associated with a shortened URL.
- * 
+ *
  * @param req - The NextApiRequest object representing the incoming request.
- * @returns A Response object with a status code of 301 and a 'Location' header set to the original URL, if successful. 
+ * @returns A Response object with a status code of 301 and a 'Location' header set to the original URL, if successful.
  *          Otherwise, a Response object with a status code of 400 and an error message.
  */
-export async function GET(req: Request){
-    try{
-        const { searchParams } = new URL(req.url)
-        const shortenedUrl = searchParams.get('shortenedUrl') 
-    
-        if(!shortenedUrl) throw new Error('no url provided')
+export async function GET(req: Request) {
+  try {
+    console.log('asdasd');
+    const { searchParams } = new URL(req.url);
+    const shortenedUrl = searchParams.get('shortenedUrl');
 
-        const originalUrl =  await redisClient.get(shortenedUrl)
+    if (!shortenedUrl) throw new Error('no url provided');
 
-        return new Response("success", { status:301, headers: {'Location': originalUrl || ''}})
-    }catch(e){
-        return  new Response(e?.message,{status:400})
-    } 
+    const originalUrl = await redisClient.get(shortenedUrl);
+
+    return new Response('success', {
+      status: 301,
+      headers: { Location: originalUrl || '' },
+    });
+  } catch (e) {
+    return new Response(e?.message, { status: 400 });
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 /**
  * Handles the POST request to create a shortened URL.
- * 
+ *
  * @param req - The NextApiRequest object representing the incoming request.
  * @returns A Promise that resolves to a NextResponse object representing the response.
  * @throws If no original URL is provided, an error is thrown.
- */ 
-export async function POST(req: Request){
-    try{
-        const { searchParams } = new URL(req.url)
-        const originalUrl = searchParams.get('originalUrl') 
-    
-        if(!originalUrl) throw new Error('no url provided')
-    
-        const nanoidId = nanoid()
-    
-        redisClient.set(nanoidId, originalUrl)
-    
-        return  Response.json({message: 'success', shortenedUrl: 'http://localhost:3000/api/urlShortner?shortenedUrl='+nanoidId})
-    }catch(e){
-        return  new Response(e?.message,{status:400})
-    } 
-} 
+ */
+export async function POST(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const originalUrl = searchParams.get('originalUrl');
+
+    if (!originalUrl) throw new Error('no url provided');
+
+    const nanoidId = nanoid();
+
+    redisClient.set(nanoidId, originalUrl);
+
+    return Response.json({
+      message: 'success',
+      shortenedUrl:
+        'http://localhost:3000/api/urlShortner?shortenedUrl=' + nanoidId,
+    });
+  } catch (e) {
+    return new Response(e?.message, { status: 400 });
+  }
+}
